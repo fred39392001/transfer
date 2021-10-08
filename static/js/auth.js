b3e79a869fa9532d5d79ec9eaa1b7601b27b93d5
yillkid <yillkid@gmail.com>
2021-10-08 20:54:12 +0800

    feat: auth check

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,6 +1,6 @@
-function checkAuth() {
-  if (getCookie("jwt") == "")
-  {
+function checkAuth(e) {
+  if (getCookie("jwt") == "") {
+    console.log("Null value of JWT");
     var path = window.location.pathname;
     var page = path.split("/").pop();
 
@@ -9,6 +9,33 @@ function checkAuth() {
     }
 
   } else {
-    console.log("jwt = " + getCookie("jwt"));
+
+    // TODO: Verify token
+    console.log("Verifing JWT ...");
+    var dataJSON = {};
+    dataJSON.token =  getCookie("jwt");
+    e.preventDefault();
+    $.ajax({
+      url: "https://eid-backend.townway.com.tw/accounts/verify_jwt",
+      type: "POST",
+      crossDomain: true,
+      data:  dataJSON,
+      success: function(returnData) {
+        const obj = JSON.parse(returnData);
+
+	if (obj.result == "True") {
+	  console.log("JWT still avliable");
+          setCookie("jwt", obj.token, 1);
+        } else {
+          window.location.replace("/eid-web/accounts/signin.html");
+	}
+
+      },
+      error: function(xhr, ajaxOptions, thrownError){
+        console.log(thrownError);
+      }
+    }); 
+
+    // console.log("jwt = " + getCookie("jwt"));
   }
 }
